d8d02c57cd0b46950546831df3db3d17d5c1d5d0
WooGeun-Nam <-03@DESKTOP-88HGK66>
2022-12-22 08:52:31 +0900

    default setting

---
+++ java-practice/src/main/java/prob1/ArrayUtilTest.java
@@ -0,0 +1,18 @@
+package prob1;
+
+import java.util.Arrays;
+
+public class ArrayUtilTest {
+
+	public static void main(String[] args) {
+		
+		double[] d = ArrayUtil.intToDouble(new int[]{10, 20, 30, 40});
+		System.out.println(Arrays.toString(d));
+		
+		int[] i1 = ArrayUtil.doubleToInt(new double[] {10.0, 11.1, 22.2, 33.4});
+		System.out.println(Arrays.toString(i1));
+		
+		int[] i2 = ArrayUtil.concat(new int[] {1, 2, 3}, new int[] {4, 5, 6});
+		System.out.println(Arrays.toString(i2));
+	}
+}

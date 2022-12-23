33693542736a2662184619e4fcde63f54fd88271
JSW <JSW@DESKTOP-JSW>
2022-12-23 20:50:38 +0900

    first prob2

--- java-practice/src/main/java/prob2/MyStack.java
+++ java-practice/src/main/java/prob2/MyStack.java
@@ -1,4 +1,27 @@
 package prob2;
 
 public class MyStack {
+	private int index;
+	private String[] str;
+	
+	public MyStack(int num) {
+		index = -1;
+		str = new String[100];
+	}
+	
+	public void push(String str) {
+		index++;
+		this.str[index] = str;
+	}
+	public String pop() throws MyStackException {
+		if(index<0) {
+			throw new MyStackException("stack is empty\"");
+		}
+		index--;
+		return str[index+1].toString();
+	}
+	public boolean isEmpty() {
+		if(index==-1) return true;
+		else return false;
+	}
 }
---
+++ java-practice/src/main/java/prob2/MyStackException.java
@@ -0,0 +1,9 @@
+package prob2;
+
+public class MyStackException extends Exception {
+
+	public MyStackException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}

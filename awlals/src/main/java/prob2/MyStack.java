0745f01e6d0c98dc6db6a8886884f1eb1aec0bcf
alswlals <awlals616@gmail.com>
2022-12-23 20:49:27 +0900

    practice05

---
+++ awlals/src/main/java/prob2/MyStack.java
@@ -0,0 +1,33 @@
+package prob2;
+//String으로 구현 
+public class MyStack {
+
+	private String[] buffer;
+	private int arr;
+	
+	public MyStack(int i) {
+		buffer = new String[i * 2];
+		arr = -1;
+	}
+
+	public void push(String string) {
+		arr += 1;
+		buffer[arr] = string;
+//		System.out.println(buffer[arr]);
+		
+	}
+
+	public String pop() throws MyStackException {
+		if(arr == -1) {
+			throw new MyStackException();
+		}
+		String s = buffer[arr];
+		arr -= 1;
+		return s;
+	}
+
+	public boolean isEmpty() {
+		return arr ==-1 ? true : false;
+	}
+	
+}
---
+++ awlals/src/main/java/prob2/MyStackException.java
@@ -0,0 +1,8 @@
+package prob2;
+
+public class MyStackException extends Exception {
+	public MyStackException() {
+		super("stack is empty");
+	}
+	
+}
